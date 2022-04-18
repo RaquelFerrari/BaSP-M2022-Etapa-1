20db079c5f7f8baeb9d370b05aa92cee96fa9de8
RaquelFerrari <raqueferrari@gmail.com>
2022-04-18 17:37:11 -0300

    BaSP-2022: ex01-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,14 @@
+var x1 = 20;
+var x2 = 9;
+var sumA = x1 + x2;
+console.log("ex01-variables and operators-A", sumA)
+
+var name1= "Radium "
+var name2= "Rocket"
+var name3= name1 + name2
+console.log("ex01-variables and operators-B", name3)
+
+var pop="Smileymiley"
+var queen="Cyrus"
+var PopQueen = pop.length + queen.length
+console.log("ex01-variables and operators-C", PopQueen)
--- readme.md
+++ readme.md
@@ -11,4 +11,9 @@ Programming landing page from Figma with HTML
 ## Week 3
 Styling landing with CSS (without flexbox)
 
+## Week 4
+Styling landing with CSS FLEXBOX 
+
+https://raquelferrari.github.io/BaSP-M2022-Etapa-1/Semana-04/
+
 ### By Raquel Ferrari
