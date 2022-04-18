53c272c9a8bc56eddd109e1bb7ba244b4a156537
RaquelFerrari <raqueferrari@gmail.com>
2022-04-18 18:46:45 -0300

    BaSP-2022:ex02-strings

---
+++ Semana-05/exercises/02_strings.js
@@ -0,0 +1,10 @@
+var myName="Raquel Ferrari"
+console.log("ex02-strings-A", myName.toUpperCase())
+
+var myProfession="Ingeniera Industrial"
+var degree= myProfession.substring(0,5)
+console.log("ex02-strings-B", degree)
+
+var myHobby="Listening to music"
+var y= myHobby.substring(15,18)
+console.log("ex02-strings-C", y)
