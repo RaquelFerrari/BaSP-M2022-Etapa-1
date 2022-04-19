1dd2cb143ca2dd3491f6d27fe42131a46d58cf4c
RaquelFerrari <raqueferrari@gmail.com>
2022-04-19 00:20:26 -0300

    BaSP-2022: ex02-strings complete

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -8,3 +8,18 @@ console.log("ex02-strings-B", degree)
 var myHobby="Listening to music"
 var y= myHobby.substring(15,18)
 console.log("ex02-strings-C", y)
+
+var myPurpose= "to be happy"
+var z= myPurpose.substring(0,1)
+var z1=myPurpose.substring(1,11)
+console.log("ex02-strings-D", z.toUpperCase()+z1.toLowerCase())
+
+var myDog= "My dog's name is Meryl"
+var myDog1= myDog.indexOf (" ")
+console.log("ex02-strings-E", myDog1)
+
+var serie1="howtogetaway withmurder"
+var serie2= serie1.substring(0,1).toUpperCase() + serie1.substring(1,12).toLowerCase() + 
+serie1.substring((serie1.indexOf(" ")),13) + serie1.substring(13,14).toUpperCase() + 
+serie1.substring(14,23).toLowerCase()
+console.log("ex02-strings-F",serie2)
---
+++ Semana-05/exercises/03_arrays.js
@@ -0,0 +1,3 @@
+var array1= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log("ex03-arrays-A", array1[5],array1[11])
