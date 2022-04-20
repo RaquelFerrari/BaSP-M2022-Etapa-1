318ea68e7110bc7b74cf6d972de3b73fd5501842
RaquelFerrari <raqueferrari@gmail.com>
2022-04-19 23:46:33 -0300

    BaSP-2022: readme

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -1,25 +1,25 @@
-// // ex05-for-A
-// var cantantes=["miley Cyrus", "Taylor Swift", "Ed Sheeran", "Adele", "Bruno Mars"]
-// for (var i=0; i < cantantes.length; i++ ){
-//     alert(cantantes[i]);
-// }
+// ex05-for-A
+var cantantes=["miley Cyrus", "Taylor Swift", "Ed Sheeran", "Adele", "Bruno Mars"]
+for (var i=0; i < cantantes.length; i++ ){
+    alert(cantantes[i]);
+}
 
-// // ex05-for-B
-// for (var i=0; i < cantantes.length; i++ ){
-//     cantantes[i]= cantantes[i].charAt(0).toUpperCase() + cantantes[i].substring(1,50)
-//     alert("These words have changed: " + cantantes[i]);
-// }
+// ex05-for-B
+for (var i=0; i < cantantes.length; i++ ){
+    cantantes[i]= cantantes[i].charAt(0).toUpperCase() + cantantes[i].substring(1,50)
+    alert("These words have changed: " + cantantes[i]);
+}
 
-// // ex05-for-C
-// var sentence= "";
-// for (var i=0; i < cantantes.length; i++){
-//     sentence = sentence + cantantes[i] + " ";
-// }
-// alert("My top5 pop singers are: " + sentence)
+// ex05-for-C
+var sentence= "";
+for (var i=0; i < cantantes.length; i++){
+    sentence = sentence + cantantes[i] + " ";
+}
+alert("My top5 pop singers are: " + sentence)
 
-// //ex05-for-D
-// var billboard=[];
-// for (i=0; i<=9; i++){
-//     billboard [i] = i
-// }
-// console.log("ex05-for-D: ", billboard)
+//ex05-for-D
+var billboard=[];
+for (i=0; i<=9; i++){
+    billboard [i] = i
+}
+console.log("ex05-for-D: ", billboard)
